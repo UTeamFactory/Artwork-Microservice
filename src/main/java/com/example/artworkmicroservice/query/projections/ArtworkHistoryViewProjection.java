5c937158815e72237ab51392c62592208426dec4
Augusto Morante <augustomorante@hotmail.com>
2022-06-01 01:50:56 -0500

    query updated

--- src/main/java/com/example/artworkmicroservice/query/projections/ArtworkHistoryViewProjection.java
+++ src/main/java/com/example/artworkmicroservice/query/projections/ArtworkHistoryViewProjection.java
@@ -1,4 +1,51 @@
 package com.example.artworkmicroservice.query.projections;
 
+import com.example.artworkmicroservice.contracts.events.ArtworkEdited;
+import com.example.artworkmicroservice.contracts.events.ArtworkRegistered;
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
 public class ArtworkHistoryViewProjection {
+    private final ArtworkHistoryViewRepository artworkHistoryViewRepository;
+
+    public ArtworkHistoryViewProjection(ArtworkHistoryViewRepository artworkHistoryViewRepository){
+        this.artworkHistoryViewRepository = artworkHistoryViewRepository;
+    }
+
+    @EventHandler
+    public void on(ArtworkRegistered event){
+        ArtworkHistoryView artworkHistoryView = new ArtworkHistoryView(
+                event.getId(),
+                event.getTitle(),
+                event.getDescription(),
+                event.getLink(),
+                event.getImage(),
+                event.getPrice(),
+                event.getOccurredOn()
+        );
+        artworkHistoryViewRepository.save(artworkHistoryView);
+    }
+
+    @EventHandler
+    public void on(ArtworkEdited event){
+        Optional<ArtworkHistoryView> artworkHistoryViewOptional = artworkHistoryViewRepository.getLastById(event.getId().toString());
+        if (artworkHistoryViewOptional.isPresent()){
+            ArtworkHistoryView artworkHistoryView = artworkHistoryViewOptional.get();
+            artworkHistoryView = new ArtworkHistoryView(artworkHistoryView);
+
+            artworkHistoryView.setTitle(event.getTitle());
+            artworkHistoryView.setDescription(event.getDescription());
+            artworkHistoryView.setLink(event.getLink());
+            artworkHistoryView.setImage(event.getImage());
+            artworkHistoryView.setPrice(event.getPrice());
+            artworkHistoryView.setCreatedAt(event.getOccurredOn());
+
+            artworkHistoryViewRepository.save(artworkHistoryView);
+        }
+    }
+
+
 }
--- src/main/java/com/example/artworkmicroservice/query/projections/ArtworkHistoryViewRepository.java
+++ src/main/java/com/example/artworkmicroservice/query/projections/ArtworkHistoryViewRepository.java
@@ -1,9 +1,24 @@
 package com.example.artworkmicroservice.query.projections;
 
+import com.example.artworkmicroservice.command.domain.entities.Artwork;
+import org.hibernate.query.spi.StreamDecorator;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ArtworkHistoryViewRepository extends JpaRepository<ArtworkHistoryView,String> {
+
+    @Query(value = "SELECT *" +
+            "       FROM artwork_history_view" +
+            "       WHERE artwork_history_id" +
+            "           =(SELECT MAX (artwork_history_id)" +
+            "               FROM artwork_history_id WHERE artwork_id = :artwork_id)",nativeQuery = true)
+    Optional<ArtworkHistoryView> getLastById(String id);
+
+    @Query(value = "SELECT * FROM artwork_history_view WHERE artwork_id = :artworkId ORDER BY created_at",nativeQuery = true)
+    List<ArtworkHistoryView> getArtworkHistoryById(String id);
 }
