81cc4fb451b676b430eb866fdc0bfb61ea5334cd
Sebastian Gonzales <sgabad@hotmail.com>
2022-06-03 21:17:22 -0500

    Added configuration

--- src/main/java/com/example/artworkmicroservice/config/SwaggerConfig.java
+++ src/main/java/com/example/artworkmicroservice/config/SwaggerConfig.java
@@ -1,4 +1,23 @@
 package com.example.artworkmicroservice.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Customers microservice")
+                        .description("Customers microservice")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Customers microservice")
+                        .url("https://github.com/George2811/server-side-software-sw51"));
+    }
 }
